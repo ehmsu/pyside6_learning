3865a3d532eb493a8a67d77816ee4ac240f3249c
ehmsu <amelydotsu@gmail.com>
2024-03-12 12:42:52 -0400

    learning QML

---
+++ more_qml_practice.py
@@ -0,0 +1,16 @@
+QML ="""
+import QtQuick
+
+Rectangle {
+    id: page
+    width: 320; height: 480 
+    color: "lightgray" 
+    
+    Text {
+        id: helloText 
+        text: "Hello world!"
+        y: 30 
+        anchors.horizontalCenter: page.horizontalCenter
+        font.pointSize: 24; font.bold: true
+    }
+}"""
